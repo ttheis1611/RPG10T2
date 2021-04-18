2faa3ae37e7577f384ca2154855661b4b23ac301
Terry Theis <tltheis@prodigy.net>
2021-04-18 11:30:34 -0500

    test Player

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,18 +1,16 @@
 const Potion = require('../lib/Potion.js');
 
-test('create a health potion object', () => {
-    const potion = new Potion('health');
+test('creates a health potion object', () => {
+  const potion = new Potion('health');
 
-    expect(potion.name).toBe('health');
-    expect(potion.value).toStrictEqual(expect.any(Number));
-
-})
+  expect(potion.name).toBe('health');
+  expect(potion.value).toEqual(expect.any(Number));
+});
 
 test('creates a random potion object', () => {
-    const potion = new Potion();
+  const potion = new Potion();
 
-    expect(potion.name).toEqual(expect.any(String));
-    expect(potion.name.length).toBeGreaterThan (0);
-    expect(potion.value).toEqual(expect.any(Number));
-
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
 });
